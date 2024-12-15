2698f34eda833e717428c3bf29b595bb966dd72c
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-12-15 20:18:51 +0530

    v1.02

--- client/src/store/Link.ts
+++ client/src/store/Link.ts
@@ -1,4 +1,4 @@
-const LINK:string = "https://chatroom-re0i.onrender.com/";
+const LINK:string = "https://chatroom-1-pvrx.onrender.com";
 // const LINK:string = "http://localhost:5000/";
 
 export default LINK;
