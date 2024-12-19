51c62205e7d1bbdf8592f7fa9d8805c34c85e1d3
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-12-19 15:13:59 +0530

    Fixed Loader Code

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -2,7 +2,12 @@ import RotateLoader from "react-spinners/RotateLoader";
 
 function Loader() {
     const serverMessage = "The Server Can Take Upto 90 Seconds Due To Inactivity";
-    return <div className="mx-5 text-center"><div className="w-full h-90vh flex flex-col justify-center items-center"><RotateLoader color="#000000" margin={12} size={25} /><h2 className="text-2xl mb-2 mt-8">{serverMessage}</h2></div></div>;
+    return <div className="mx-5 text-center">
+        <div className="w-full h-80vh flex flex-col justify-center items-center">
+            <RotateLoader color="#000000" margin={12} size={25} />
+            <h2 className="text-2xl mb-2 mt-8">{serverMessage}</h2>
+        </div>
+    </div>;
     
 }
 
