7ea247f7754e0ba21c6ecf9d1dc31d04af6cd459
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-12-22 22:36:32 +0530

    removed comments

--- server/index.js
+++ server/index.js
@@ -43,17 +43,14 @@ app.get('/', (req, res) => {
 io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
-    // Event for joining a chatroom
     socket.on("joinChatroom", (chatroomName) => {
-        socket.join(chatroomName); // Join specific chatroom
+        socket.join(chatroomName);
         console.log(`${socket.id} joined chatroom: ${chatroomName}`);
     });
 
-    // Event for sending a message
     socket.on("sendMessage", async ({ chatroomName, message, userId, username }) => {
         console.log(`Message: "${message}"\nChatroom: ${chatroomName}`);
 
-        // Save message to MongoDB
         const newMessage = {
             chatroomId: chatroomName,
             userId,
@@ -61,13 +58,11 @@ io.on("connection", (socket) => {
             message,
             timestamp: new Date(),
         };
-        await Chat.create(newMessage); // Ensure the Chat model is correctly imported.
+        await Chat.create(newMessage);
 
-        // Emit message to users in the same chatroom
         io.to(chatroomName).emit("receiveMessage", newMessage);
     });
 
-    // Event for user disconnect
     socket.on("disconnect", () => {
         console.log("User disconnected:", socket.id);
     });
@@ -78,65 +73,3 @@ connectDB().then( () => {
         console.log(`Server is running at Port ${PORT}`);
     })
 });
-
-
-// import express from "express";
-// import http from "http";
-// import { Server} from "socket.io";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import router from "./router/auth-router.js";
-// import {connectDB} from "./utils/db.js"
-// import errorMiddleware from "./middlewares/error-middleware.js";
-// import chatroomRouter from "./router/chatroom-router.js"
-// import chatRouter from "./router/chat-router.js";
-
-
-// const PORT= 5000; 
-// const app = express();
-
-// app.use(cors({
-//     origin: "*",
-//     methods: "*",
-//     allowedHeaders: "*",
-//     exposedHeaders: "*",
-//     credentials: true,
-// }));
-// app.use(express.json());
-// app.use("/api/auth", router);
-// app.use("/api/chatroom", chatroomRouter);
-// app.use("/api/chat", chatRouter);
-// app.use(errorMiddleware);
-
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*", 
-//         methods: "*", 
-//     },
-// });
-
-// io.on("connection", (socket) => {
-//     console.log("A user connected:", socket.id);
-
-//     socket.on("joinChatroom", (chatroomName) => {
-//         socket.join(chatroomName);
-//         console.log(`${socket.id} joined chatroom: ${chatroomName}`);
-//     });
-
-//     socket.on("sendMessage", ({ chatroomName, message, username}) => {
-//         console.log("Message: ", message, "\tChatroom: ",chatroomName, "\tUsername: ", username);
-//         io.to(chatroomName).emit("receiveMessage", { message });
-//     });
-
-//     socket.on("disconnect", () => {
-//         console.log("User disconnected:", socket.id);
-//     });
-// });
-
-// connectDB().then( () => {
-//     server.listen(PORT, ()=> {
-//         console.log(`Server is running at Port ${PORT}`);
-//     })
-// });
