8d1af4cc5f44549847fa68c58f460504ede62b4a
rajasimha23 <rajasimha2301@gmail.com>
2025-01-03 21:38:35 +0530

    only copy files

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,12 @@ ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" "mkdir -p $APP_DIR"
 # Transfer server and client files to the EC2 instance
 scp -o StrictHostKeyChecking=no -i "$1" -r server ubuntu@"$2":"$APP_DIR"/server
 scp -o StrictHostKeyChecking=no -i "$1" -r client ubuntu@"$2":"$APP_DIR"/client
- 
+
+###
 # Install dependencies and restart the backend
-ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" << 'EOF'
-cd $APP_DIR/server
-npm install --include=dev
-pm2 stop all || true  
-pm2 start server.js --name "chatroom-backend"
-EOF
+#ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" << 'EOF'
+#cd $APP_DIR/server
+#npm install --include=dev
+#pm2 stop all || true  
+#pm2 start server.js --name "chatroom-backend" &
+#EOF
