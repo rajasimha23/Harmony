3e05a21374557bdaa0cf43400722e9aace3a8bd5
rajasimha23 <rajasimha2301@gmail.com>
2025-01-03 22:13:53 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,12 @@ APP_DIR=/home/ubuntu/chatroom-app
 # Create application directory on EC2 instance
 ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" "mkdir -p $APP_DIR"
 
+rsync -avz --exclude 'node_modules' ./server ubuntu@"$EC2_IP":"$APP_DIR"/server
+rsync -avz --exclude 'node_modules' ./client ubuntu@"$EC2_IP":"$APP_DIR"/client
+
 # Transfer server and client files to the EC2 instance
-scp -o StrictHostKeyChecking=no -i "$1" -r server ubuntu@"$2":"$APP_DIR"/server
-scp -o StrictHostKeyChecking=no -i "$1" -r client ubuntu@"$2":"$APP_DIR"/client
+#scp -o StrictHostKeyChecking=no -i "$1" -r server ubuntu@"$2":"$APP_DIR"/server
+#scp -o StrictHostKeyChecking=no -i "$1" -r client ubuntu@"$2":"$APP_DIR"/client
 
 ###
 # Install dependencies and restart the backend
