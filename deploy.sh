0886a7e774eb0da0e99ce61d9b844cb65c84ccff
rajasimha23 <rajasimha2301@gmail.com>
2025-01-03 21:01:56 +0530

    ec2user changed to ubuntu

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@
 APP_DIR=/home/ec2-user/chatroom-app
 
 # Create application directory on EC2 instance
-ssh -o StrictHostKeyChecking=no -i "$1" ec2-user@"$2" "mkdir -p $APP_DIR"
+ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" "mkdir -p $APP_DIR"
 
 # Transfer server and client files to the EC2 instance
-scp -o StrictHostKeyChecking=no -i "$1" -r server ec2-user@"$2":"$APP_DIR"/server
-scp -o StrictHostKeyChecking=no -i "$1" -r client ec2-user@"$2":"$APP_DIR"/client
+scp -o StrictHostKeyChecking=no -i "$1" -r server ubuntu@"$2":"$APP_DIR"/server
+scp -o StrictHostKeyChecking=no -i "$1" -r client ubuntu@"$2":"$APP_DIR"/client
  
 # Install dependencies and restart the backend
-ssh -o StrictHostKeyChecking=no -i "$1" ec2-user@"$2" << 'EOF'
+ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" << 'EOF'
 cd $APP_DIR/server
 npm install --include=dev
 pm2 stop all || true  
