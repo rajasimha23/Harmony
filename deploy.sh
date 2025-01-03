f0e99e15f786a49c1c896b8c72d67c42d0c1008c
rajasimha23 <rajasimha2301@gmail.com>
2025-01-03 19:32:45 +0530

    Created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Variables
+APP_DIR=/home/ec2-user/mern-app
+
+# Create application directory on EC2 instance
+ssh -o StrictHostKeyChecking=no -i "$1" ec2-user@"$2" "mkdir -p $APP_DIR"
+
+# Transfer server and frontend files to the EC2 instance
+scp -o StrictHostKeyChecking=no -i "$1" -r server ec2-user@"$2":"$APP_DIR"/
+scp -o StrictHostKeyChecking=no -i "$1" -r client/build ec2-user@"$2":"$APP_DIR"/client
+
+# Install dependencies and restart the backend
+ssh -o StrictHostKeyChecking=no -i "$1" ec2-user@"$2" << 'EOF'
+cd $APP_DIR/server
+npm install --production
+pm2 stop all || true  
+pm2 start server.js --name "chatroom-backend"
