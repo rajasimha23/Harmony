5c83df5b804f978e4c06feae79cc7311cd6b4196
rajasimha23 <rajasimha2301@gmail.com>
2025-01-04 00:31:49 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,18 @@ APP_DIR=/home/ubuntu/chatroom-app
 # Create application directory on EC2 instance
 ssh -o StrictHostKeyChecking=no -i "$1" ubuntu@"$2" "mkdir -p $APP_DIR"
 
-rsync -avz --exclude 'node_modules' ./server ubuntu@"$2":"$APP_DIR"/server
-rsync -avz --exclude 'node_modules' ./client ubuntu@"$2":"$APP_DIR"/client
+tar --exclude='node_modules' -czf client.tar.gz ./client
+tar --exclude='node_modules' -czf server.tar.gz ./server
+
+scp -i "$1" client.tar.gz ubuntu@"$2":"$APP_DIR"/client
+scp -i "$1" server.tar.gz ubuntu@"$2":"$APP_DIR"/server
+
+ssh -i "$1" ubuntu@"$2" "tar -xzf $APP_DIR/client/client.tar.gz -C $APP_DIR/client"
+ssh -i "$1" ubuntu@"$2" "tar -xzf $APP_DIR/server/server.tar.gz -C $APP_DIR/server"
+
+
+#rsync -avz --exclude 'node_modules' ./server ubuntu@"$2":"$APP_DIR"/server
+#rsync -avz --exclude 'node_modules' ./client ubuntu@"$2":"$APP_DIR"/client
 
 # Transfer server and client files to the EC2 instance
 #scp -o StrictHostKeyChecking=no -i "$1" -r server ubuntu@"$2":"$APP_DIR"/server
